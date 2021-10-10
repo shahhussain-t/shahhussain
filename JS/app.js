f9784410199413dcf99d6a2c07eec031d2ee7baf
shah <shahhussaint6@gmail.com>
2021-10-10 19:29:27 +0500

    finalizing

--- JS/app.js
+++ JS/app.js
@@ -1,24 +1,99 @@
 const menuBtn = document.querySelector(".menu-btn");
 const navbar = document.querySelector(".navBar");
-const tole = document.querySelector(".menu-btn-burger");
+const list =document.querySelector(".list")
+const contactMe =document.querySelector("#contactMe");
+const sliders=document.querySelectorAll(".slide-in");
+const slideUp=document.querySelector(".slide-up");
+
 
 let btn = false;
-menuBtn.addEventListener("click", () => {
-  if (btn) {
-    navbar.classList.replace("open", "close");
-    menuBtn.classList.remove('open')
 
-    setTimeout(() => {
-      navbar.classList.replace("close", "ab");
-    }, 1000);
 
-    btn = false;
-  } else {
-    navbar.classList.replace("ab", "open");
-    navbar.classList.replace("close", "open");
-    menuBtn.classList.add('open')
-    btn = true;
+contactMe.addEventListener("click",()=>{
+  
+  navbar.classList.remove("open")
+    menuBtn.classList.remove("open")
+    btn=false
+  
+  
+})
+
+list.addEventListener("click",(e)=>{
+  
+ 
+  if(e.target)
+  {
+    
+    navbar.classList.remove("open")
+    menuBtn.classList.remove("open")
+    btn=false
+  
   }
   
-  tole.classList.toggle("open")
+  var elems = document.querySelectorAll(".active");
+  [].forEach.call(elems, function(el) {
+    el.classList.remove("active");
+  });
+  e.target.parentElement.classList.add("active")
+  console.log(e.target.parentElement)
+})
+
+menuBtn.addEventListener("click", () => {
+ 
+  if(btn){
+    
+    navbar.classList.remove("open")
+    menuBtn.classList.remove("open")
+    btn=false
+  }else{
+    
+    navbar.classList.add("open")
+    menuBtn.classList.add("open")
+    btn=true
+  }
+  
+  
 });
+
+// observer
+
+const appearOptions={
+  
+threshold:0,
+rootMargin:"0px 0px -200px 0px"
+  
+  
+}
+
+const appearOnScroll=new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+  
+){
+  
+  entries.forEach(entry=>{
+    
+    if(!entry.isIntersecting){
+      
+      return;
+      
+    }else{
+      
+      entry.target.classList.add('appear')
+      appearOnScroll.unobserve(entry.target)
+      
+    }
+    
+  })
+  
+},appearOptions)
+
+
+sliders.forEach(slider=>{
+  
+  
+  appearOnScroll.observe(slider)
+  
+})
+
+appearOnScroll.observe(slideUp)
