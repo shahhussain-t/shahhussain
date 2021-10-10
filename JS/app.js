965111ca86bea53cd4b972626cf5e3941a019780
shah <shahhussaint6@gmail.com>
2021-10-10 19:45:55 +0500

    solve hidden issu

--- JS/app.js
+++ JS/app.js
@@ -1,99 +1,104 @@
-const menuBtn = document.querySelector(".menu-btn");
-const navbar = document.querySelector(".navBar");
-const list =document.querySelector(".list")
-const contactMe =document.querySelector("#contactMe");
-const sliders=document.querySelectorAll(".slide-in");
-const slideUp=document.querySelector(".slide-up");
 
 
-let btn = false;
-
-
-contactMe.addEventListener("click",()=>{
+window.addEventListener('DOMContentLoaded', (event) => {
   
-  navbar.classList.remove("open")
-    menuBtn.classList.remove("open")
-    btn=false
+  const menuBtn = document.querySelector(".menu-btn");
+  const navbar = document.querySelector(".navBar");
+  const list =document.querySelector(".list")
+  const contactMe =document.querySelector("#contactMe");
+  const sliders=document.querySelectorAll(".slide-in");
+  const slideUp=document.querySelector(".slide-up");
+  let btn = false;
   
   
-})
-
-list.addEventListener("click",(e)=>{
   
- 
-  if(e.target)
-  {
+  contactMe.addEventListener("click",()=>{
     
     navbar.classList.remove("open")
-    menuBtn.classList.remove("open")
-    btn=false
-  
-  }
-  
-  var elems = document.querySelectorAll(".active");
-  [].forEach.call(elems, function(el) {
-    el.classList.remove("active");
-  });
-  e.target.parentElement.classList.add("active")
-  console.log(e.target.parentElement)
-})
-
-menuBtn.addEventListener("click", () => {
- 
-  if(btn){
+      menuBtn.classList.remove("open")
+      btn=false
     
-    navbar.classList.remove("open")
-    menuBtn.classList.remove("open")
-    btn=false
-  }else{
-    
-    navbar.classList.add("open")
-    menuBtn.classList.add("open")
-    btn=true
-  }
-  
-  
-});
-
-// observer
-
-const appearOptions={
-  
-threshold:0,
-rootMargin:"0px 0px -200px 0px"
-  
-  
-}
-
-const appearOnScroll=new IntersectionObserver(function(
-  entries,
-  appearOnScroll
-  
-){
-  
-  entries.forEach(entry=>{
-    
-    if(!entry.isIntersecting){
-      
-      return;
-      
-    }else{
-      
-      entry.target.classList.add('appear')
-      appearOnScroll.unobserve(entry.target)
-      
-    }
     
   })
   
-},appearOptions)
-
-
-sliders.forEach(slider=>{
+  list.addEventListener("click",(e)=>{
+    
+   
+    if(e.target)
+    {
+      
+      navbar.classList.remove("open")
+      menuBtn.classList.remove("open")
+      btn=false
+    
+    }
+    
+    var elems = document.querySelectorAll(".active");
+    [].forEach.call(elems, function(el) {
+      el.classList.remove("active");
+    });
+    e.target.parentElement.classList.add("active")
+    console.log(e.target.parentElement)
+  })
+  
+  menuBtn.addEventListener("click", () => {
+   
+    if(btn){
+      
+      navbar.classList.remove("open")
+      menuBtn.classList.remove("open")
+      btn=false
+    }else{
+      
+      navbar.classList.add("open")
+      menuBtn.classList.add("open")
+      btn=true
+    }
+    
+    
+  });
+  
+  // observer
+  
+  const appearOptions={
+    
+  threshold:0,
+  rootMargin:"0px 0px -200px 0px"
+    
+    
+  }
+  
+  const appearOnScroll=new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+    
+  ){
+    
+    entries.forEach(entry=>{
+      
+      if(!entry.isIntersecting){
+        
+        return;
+        
+      }else{
+        
+        entry.target.classList.add('appear')
+        appearOnScroll.unobserve(entry.target)
+        
+      }
+      
+    })
+    
+  },appearOptions)
   
   
-  appearOnScroll.observe(slider)
+  sliders.forEach(slider=>{
+    
+    
+    appearOnScroll.observe(slider)
+    
+  })
   
-})
+  appearOnScroll.observe(slideUp)
+});
 
-appearOnScroll.observe(slideUp)
